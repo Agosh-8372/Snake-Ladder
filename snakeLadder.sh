2f6fab99f8d66443c3ba3406eee3251e32677f9c
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-12 12:26:04 +0530

    [agosh]DELETE:Extra code removed as per use case

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,4 +1,4 @@
-START_POSITION=0;
+START_POSITION=0; 
 FINAL_POSITION=100;
 
 playerPos1=0;
@@ -13,8 +13,6 @@ function rollDie()
 
 function play()
 {
-	while (( $playerPos1 < $FINAL_POSITION ))
-	do
 		random=$((RANDOM%3))
 			case $random in
 				0)
@@ -22,32 +20,14 @@ function play()
 					       	;;
 				1)
 		      	count=$(rollDie)
-					reqValue=$((FINAL_POSITION-playerPos1))
-						if [ $reqValue -gt $dieCount ]
-						then
-			     			playerPos1=$((playerPos1+count))
-							echo "You got Ladder: Dice value=$count & player position="$playerPos1
-						elif [ $reqValue -le $dieCount ] && [ $count -le $reqValue ]
-						then
-							playerPos1=$((playerPos1+count))
-							echo "You got Ladder: Dice value=$count & player position="$playerPos1
-						else
-							echo "You got Ladder: Dice value=$count & player position="$playerPos1
-						fi
+					playerPos1=$((playerPos1+count))
+					echo "You got Ladder: Dice value=$count & player position="$playerPos1
 								;;
-			2)
+				2)
 					count=$(rollDie)
-						if [ $playerPos1 -eq $START_POSITION ]||[ $playerPos1 -lt $count ]
-						then
-            			playerPos1=0;
-							echo "Got a Snake: Dice value=$count & player position="$playerPos1
-						else
-							playerPos1=$((playerPos1-count))
-							echo "Got a Snake: Dice value=$count & player position="$playerPos1
-
-						fi
+					playerPos1=$((playerPos1-count))
+					echo "Got a Snake: Dice value=$count & player position="$playerPos1
 								;;
 			esac
-	done
 }
 play
