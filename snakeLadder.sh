6c013084b9c7ef4d7df64fdc3a0d47832dc5a7d5
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-12 10:06:33 +0530

    [agosh]ADD:Conditions for No Play,Ladder & Snake

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,12 +1,55 @@
-#!/bin/bash -x
 START_POSITION=0;
 FINAL_POSITION=100;
 P1_START_POS=0;
 
+playerPos1=0;
+dieCount=6;
+
 function rollDie()
 {
-random=$(((RANDOM%6)+1))
-echo $random
+diceValue=$(((RANDOM%6)+1))
+echo $diceValue
 }
 
-rollDie
+
+function play()
+{
+	while (( $playerPos1 < $FINAL_POSITION ))
+	do
+		random=$((RANDOM%3))
+		case $random in
+			0)
+				echo "NO PLAY & player position="$playerPos1
+					          ;;
+			1)
+		      count=$(rollDie)
+				reqValue=$((FINAL_POSITION-playerPos1))
+					if [ $reqValue -gt $dieCount ]
+					then
+			     			playerPos1=$((playerPos1+count))
+							echo "You got Ladder & player position="$playerPos1
+					elif [ $reqValue -le $dieCount ] && [ $count -le $reqValue ]
+					then
+							playerPos1=$((playerPos1+count))
+							echo "You got Ladder & player position="$playerPos1
+					else
+							echo "You got Ladder & player position="$playerPos1
+					fi
+									;;
+			2)
+				count=$(rollDie)
+					if [ $playerPos1 -eq 0 ]||[ $playerPos1 -lt $count ]
+					then
+            			playerPos1=0;
+							echo "Got a Snake & player position="$playerPos1
+					else
+							playerPos1=$((playerPos1-count))
+							echo "Got a Snake & player position="$playerPos1
+
+
+					fi
+									;;
+		esac
+	done
+}
+play
