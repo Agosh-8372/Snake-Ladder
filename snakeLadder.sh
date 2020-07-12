b409904753393b8b113e2aa0ac06558c9d17e443
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-12 13:08:18 +0530

    [agosh]REFACTOR:Proper Indentation

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -2,7 +2,6 @@ START_POSITION=0;
 FINAL_POSITION=100;
 
 playerPos1=0;
-dieCount=6;
 
 function rollDie()
 {
@@ -14,22 +13,22 @@ function rollDie()
 function play()
 {
 	while (( $playerPos1 < $FINAL_POSITION ))
-   do
+	do
 		random=$((RANDOM%3))
 			case $random in
 				0)
 					echo "NO PLAY : player position="$playerPos1
 					       	;;
 				1)
-		      	count=$(rollDie)
+					count=$(rollDie)
 					playerPos1=$((playerPos1+count))
 					echo "You got Ladder: Dice value=$count & player position="$playerPos1
 								;;
 				2)
 					count=$(rollDie)
 						if [ $playerPos1 -le $START_POSITION ]
-           			then
-                 			playerPos1=0;
+						then
+								playerPos1=0;
           					echo "Got a Snake: Dice value=$count & player position="$playerPos1
 						else
 								playerPos1=$((playerPos1-count))
