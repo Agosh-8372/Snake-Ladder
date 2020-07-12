30976dc239b02b1ab40f7fad855e3ec34da79a39
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-12 12:54:14 +0530

    [agosh]ADD:player position resets to 0,if negative

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -13,6 +13,8 @@ function rollDie()
 
 function play()
 {
+	while (( $playerPos1 < $FINAL_POSITION ))
+   do
 		random=$((RANDOM%3))
 			case $random in
 				0)
@@ -25,9 +27,16 @@ function play()
 								;;
 				2)
 					count=$(rollDie)
-					playerPos1=$((playerPos1-count))
-					echo "Got a Snake: Dice value=$count & player position="$playerPos1
+						if [ $playerPos1 -le $START_POSITION ]
+           			then
+                 			playerPos1=0;
+          					echo "Got a Snake: Dice value=$count & player position="$playerPos1
+						else
+								playerPos1=$((playerPos1-count))
+								echo "Got a Snake: Dice value=$count & player position="$playerPos1
+						fi
 								;;
 			esac
+	done
 }
 play
