999d4d5ff93e7e8a076a6e881ee0f93555b267c0
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-12 19:01:50 +0530

    [agosh]ADD:Total count for dice roll & position on every roll

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -1,12 +1,15 @@
 START_POSITION=0; 
 FINAL_POSITION=100;
+DICE_VALUE_LIMIT=6;
 
 playerPos1=0;
-dieCount=6;
+diceCount=0;
+
 function rollDie()
 {
+	((++diceCount))
 	diceValue=$(((RANDOM%6)+1))
-	echo $diceValue
+
 }
 
 
@@ -20,32 +23,33 @@ function play()
 					echo "NO PLAY : player position="$playerPos1
 					       	;;
 				1)
-					count=$(rollDie)
+					rollDie
 					reqValue=$((FINAL_POSITION-playerPos1))
-							if [ $reqValue -gt $dieCount ]
+							if [ $reqValue -gt $DICE_VALUE_LIMIT ]
 							then
-								playerPos1=$((playerPos1+count))
-								echo "You got Ladder: Dice value=$count & player position="$playerPos1
-							elif [ $reqValue -le $dieCount ] && [ $count -le $reqValue ]
+								playerPos1=$((playerPos1+diceValue))
+								echo "You got Ladder: Dice value=$diceValue & player position="$playerPos1
+							elif [ $reqValue -le $DICE_VALUE_LIMIT ] && [ $diceValue -le $reqValue ]
 							then
-								playerPos1=$((playerPos1+count))
-								echo "You got Ladder: Dice value=$count & player position="$playerPos1
+								playerPos1=$((playerPos1+diceValue))
+								echo "You got Ladder: Dice value=$diceValue & player position="$playerPos1
 							else
-								echo "You got Ladder: Dice value=$count & player position="$playerPos1
+								echo "You got Ladder: Dice value=$diceValue & player position="$playerPos1
 							fi
 								;;
 				2)
-					count=$(rollDie)
-						if [ $playerPos1 -le $START_POSITION ]||[ $playerPos1 -lt $count ]
+					rollDie
+						if [ $playerPos1 -le $START_POSITION ]||[ $playerPos1 -lt $diceValue ]
 						then
 								playerPos1=0;
-          					echo "Got a Snake: Dice value=$count & player position="$playerPos1
+          					echo "Got a Snake: Dice value=$diceValue & player position="$playerPos1
 						else
-								playerPos1=$((playerPos1-count))
-								echo "Got a Snake: Dice value=$count & player position="$playerPos1
+								playerPos1=$((playerPos1-diceValue))
+								echo "Got a Snake: Dice value=$diceValue & player position="$playerPos1
 						fi
 								;;
 			esac
 	done
+echo -e "\nNumber of times dice was rolled in the game="$diceCount
 }
 play
