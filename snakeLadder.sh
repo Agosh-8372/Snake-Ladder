cf2f387ef6b8d96d79d134cc164c191066bd9b50
agosh-8372 <agoshchauhan378@gmail.com>
2020-07-12 17:10:33 +0530

    [agosh]ADD:get exact number that adds to 100 for finish

--- snakeLadder.sh
+++ snakeLadder.sh
@@ -2,7 +2,7 @@ START_POSITION=0;
 FINAL_POSITION=100;
 
 playerPos1=0;
-
+dieCount=6;
 function rollDie()
 {
 	diceValue=$(((RANDOM%6)+1))
@@ -21,12 +21,22 @@ function play()
 					       	;;
 				1)
 					count=$(rollDie)
-					playerPos1=$((playerPos1+count))
-					echo "You got Ladder: Dice value=$count & player position="$playerPos1
+					reqValue=$((FINAL_POSITION-playerPos1))
+							if [ $reqValue -gt $dieCount ]
+							then
+								playerPos1=$((playerPos1+count))
+								echo "You got Ladder: Dice value=$count & player position="$playerPos1
+							elif [ $reqValue -le $dieCount ] && [ $count -le $reqValue ]
+							then
+								playerPos1=$((playerPos1+count))
+								echo "You got Ladder: Dice value=$count & player position="$playerPos1
+							else
+								echo "You got Ladder: Dice value=$count & player position="$playerPos1
+							fi
 								;;
 				2)
 					count=$(rollDie)
-						if [ $playerPos1 -le $START_POSITION ]
+						if [ $playerPos1 -le $START_POSITION ]||[ $playerPos1 -lt $count ]
 						then
 								playerPos1=0;
           					echo "Got a Snake: Dice value=$count & player position="$playerPos1
